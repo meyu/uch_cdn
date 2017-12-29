5bf7144fe2553d567a145733ec080de9d97dfee3
meyu <yrrvknone@gmail.com>
2017-12-29 09:01:04 +0800

    upgrade to Bootstrap v4.0.0-beta.3

--- theme/navi/_base.js
+++ theme/navi/_base.js
@@ -1,15 +1,15 @@
-﻿//適用於 Bootstrap v4.0.0-beta.2
+﻿//適用於 Bootstrap v4.0.0-beta.3
 
 var js_4_bootstrap = document.getElementById("js_4_bootstrap");
 
 var popperJS = document.createElement("script");
 popperJS.type = "application/javascript";
-popperJS.src = "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js";
+popperJS.src = "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js";
 js_4_bootstrap.appendChild(popperJS);
 
 var bootstrapJS = document.createElement("script");
 bootstrapJS.type = "application/javascript";
-bootstrapJS.src = "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js";
+bootstrapJS.src = "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js";
 js_4_bootstrap.appendChild(bootstrapJS);
 
 
