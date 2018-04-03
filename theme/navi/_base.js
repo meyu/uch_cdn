1b1d85dd7fd2622094f797dbd4a357cfa9401e66
meyu <yrrvknone@gmail.com>
2018-04-03 16:32:16 +0800

    去除Metrail Design Icon，改用 font awesome

--- theme/navi/_base.js
+++ theme/navi/_base.js
@@ -12,6 +12,11 @@ bootstrapJS.type = "application/javascript";
 bootstrapJS.src = "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js";
 js_4_bootstrap.appendChild(bootstrapJS);
 
+var fontawesomeJS = document.createElement("script");
+fontawesomeJS.type = "application/javascript";
+fontawesomeJS.src = "https://use.fontawesome.com/releases/v5.0.8/js/all.js";
+js_4_bootstrap.appendChild(fontawesomeJS);
+
 
 //執行於頁面顯示時
 function onpageshow() {
@@ -22,8 +27,8 @@ function onpageshow() {
     var site_user_contact = document.getElementById("site_user_contact");
     var site_user_signout = document.getElementById("site_user_signout");
     logo_uch.innerHTML = "<span class='icon icon-uchlogo'></span>";
-    site_user_contact.innerHTML = "<i class='material-icons'>info_outline</i><span>聯絡我們</span>";
-    site_user_signout.innerHTML = "<i class='material-icons'>power_settings_new</i><span>登出</span>";
+    site_user_contact.innerHTML = "<i class='far fa-comments fa-fw'></i><span>聯絡我們</span>";
+    site_user_signout.innerHTML = "<i class='fas fa-sign-out-alt fa-fw'></i><span>登出</span>";
 
 }
 
