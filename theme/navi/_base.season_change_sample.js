30824895870a54a7f6b26997ad6c06e39337d18f
meyu <yrrvknone@gmail.com>
2018-06-12 09:28:11 +0800

    端午主題佈署

--- theme/navi/_base.season_change_sample.js
+++ theme/navi/_base.season_change_sample.js
@@ -1,39 +1,42 @@
-﻿//適用於 Bootstrap v4.0.0
+﻿//適用於 Bootstrap v4.1.0
 
 var js_4_bootstrap = document.getElementById("js_4_bootstrap");
 
 var popperJS = document.createElement("script");
 popperJS.type = "application/javascript";
-popperJS.src = "https://www.uch.edu.tw/cdn/js/popper/popper-1.12.9.min.js";
+popperJS.src = "https://www.uch.edu.tw/cdn/js/popper/popper-1.14.0.min.js";
 js_4_bootstrap.appendChild(popperJS);
 
 var bootstrapJS = document.createElement("script");
 bootstrapJS.type = "application/javascript";
-bootstrapJS.src = "https://www.uch.edu.tw/cdn/js/bootstrap/bootstrap-4.0.0.min.js";
+bootstrapJS.src = "https://www.uch.edu.tw/cdn/js/bootstrap/bootstrap-4.1.0.min.js";
 js_4_bootstrap.appendChild(bootstrapJS);
+
+var fontawesomeJS = document.createElement("script");
+fontawesomeJS.type = "application/javascript";
+fontawesomeJS.src = "https://www.uch.edu.tw/cdn/js/font_awesome/fontawesome-5.0.10.js";
+js_4_bootstrap.appendChild(fontawesomeJS);
 
 
 //執行於頁面顯示時
 function onpageshow() {
 
-   
     // 產生資訊圖標
     var logo_uch = document.getElementById("logo_uch");
     var site_user_contact = document.getElementById("site_user_contact");
     var site_user_signout = document.getElementById("site_user_signout");
     logo_uch.innerHTML = "<span class='icon icon-uchlogo'></span>";
-    site_user_contact.innerHTML = "<i class='material-icons'>info_outline</i><span>聯絡我們</span>";
-    site_user_signout.innerHTML = "<i class='material-icons'>power_settings_new</i><span>登出</span>";
+    site_user_contact.innerHTML = "<i class='far fa-comments fa-fw'></i><span>聯絡我們</span>";
+    site_user_signout.innerHTML = "<i class='fas fa-sign-out-alt fa-fw'></i><span>登出</span>";
 
     var rr = Math.floor((Math.random() * 100) + 1);
-    console.log(rr);
     if (rr % 4 == 0) {
-        // 冬季
+        // 主題更換
         $('body').append('<link rel="stylesheet" type="text/css" href="//aps4.uch.edu.tw/cdn/theme/navi/20171201.winter.css">')
     }
 
     if (rr % 3 == 0) {
-        //戴個小葉子
+        // 定位嵌入
         var hat = document.createElement("img");
         hat.src = "https://thumbs.gfycat.com/ReasonableDizzyBlackandtancoonhound-max-1mb.gif";
         hat.style.position = "absolute";
@@ -44,7 +47,7 @@ function onpageshow() {
     } 
 
     if (rr % 2 == 0) {
-        //來個雙背景
+        // 頁首底圖
         var site_info_header = document.getElementById("site_info_header");
         site_info_header.style.backgroundImage = "url('//aps4.uch.edu.tw/cdn/images/f_leaf1270.png')";
         site_info_header.style.backgroundPosition = "center";
@@ -52,7 +55,7 @@ function onpageshow() {
     }
 
     if (rr % 5 == 0)    {
-        //來個全景圖
+        // 頁面隨地
         var sponge = document.createElement("img");
         sponge.src = "//aps4.uch.edu.tw/cdn/images/f_leaf1270.png";
         sponge.style.width = "300px";
--- theme/navi/db_festival/_base.dragon_boat_2018.js
+++ theme/navi/db_festival/_base.dragon_boat_2018.js
@@ -20,7 +20,7 @@ js_4_bootstrap.appendChild(fontawesomeJS);
 
 //執行於頁面顯示時
 function onpageshow() {
-   
+
     // 產生資訊圖標
     var logo_uch = document.getElementById("logo_uch");
     var site_user_contact = document.getElementById("site_user_contact");
@@ -40,10 +40,10 @@ function onpageshow() {
     var rr = Math.floor((Math.random() * 100) + 1);
 
     // 粽子無字
-    if (rr < 25) {
+    if (rr < 30) {
         site_info_header.style.backgroundColor = "#FBB03B";
         logo_uch.innerHTML = ""
-        zongzi.src = "https://www.uch.edu.tw/cdn/theme/navi/db_festival/rise.gif";
+        zongzi.src = "/rise.gif";
         zongzi.style.position = "absolute";
         zongzi.style.top = "-10px";
         zongzi.style.left = "-25px";
@@ -52,10 +52,10 @@ function onpageshow() {
     }
 
     // 粽子有字
-    if (rr >= 25 && rr < 50) {
+    if (rr >= 30 && rr < 60) {
         site_info_header.style.backgroundColor = "#FBB03B";
         logo_uch.innerHTML = ""
-        zongzi.src = "https://www.uch.edu.tw/cdn/theme/navi/db_festival/sese.gif";
+        zongzi.src = "/sese2.gif";
         zongzi.style.position = "absolute";
         zongzi.style.top = "-5px";
         zongzi.style.left = "-15px";
@@ -64,15 +64,16 @@ function onpageshow() {
     }
 
     // 龍舟
-    if (rr >= 50) {
+    if (rr >= 60) {
         site_info_header.style.backgroundColor = "#29ABE2";
         boat.id = "dragon_boat"
-        boat.src = "https://www.uch.edu.tw/cdn/theme/navi/db_festival/boat.gif";
+        boat.src = "/boat.gif";
         boat.style.position = "absolute";
         boat.style.top = "8px";
+        boat.style.zIndex = -10;
         boat.style.height = "90px";
         site_title.appendChild(boat);
     }
-
+        
 }
 
